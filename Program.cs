037f8ea2e883b23dac4acb47fb25f47d4a51221d
Mazen Megahed <mazen.megahed1@outlook.com>
2025-07-02 22:52:51 +0300

    Question 1

--- Program.cs
+++ Program.cs
@@ -1,10 +1,21 @@
-﻿namespace Session4_Assignment
+﻿using static System.Runtime.InteropServices.JavaScript.JSType;
+
+namespace Session4_Assignment
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            #region Q1
+            // Write a program that takes a number from the user then print yes if that
+            //number can be divided by 3 and 4 otherwise print no.
+            Console.WriteLine("Enter A num ");
+            int num1 = int.Parse(Console.ReadLine());
+            if (num1 % 3 == 0 && num1 % 4 == 0)
+            { Console.WriteLine("Yes"); }
+            else { Console.WriteLine("no"); }
+            #endregion
+
         }
     }
 }
