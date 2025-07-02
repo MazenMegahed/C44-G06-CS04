1581d5a47ca5dc5531deb5ddc4dbe86aca85af42
Mazen Megahed <mazen.megahed1@outlook.com>
2025-07-02 22:57:36 +0300

    Question 2

--- Program.cs
+++ Program.cs
@@ -9,13 +9,33 @@ namespace Session4_Assignment
             #region Q1
             // Write a program that takes a number from the user then print yes if that
             //number can be divided by 3 and 4 otherwise print no.
-            Console.WriteLine("Enter A num ");
-            int num1 = int.Parse(Console.ReadLine());
-            if (num1 % 3 == 0 && num1 % 4 == 0)
-            { Console.WriteLine("Yes"); }
-            else { Console.WriteLine("no"); }
+            //Console.WriteLine("Enter A num ");
+            //int num1 = int.Parse(Console.ReadLine());
+            //if (num1 % 3 == 0 && num1 % 4 == 0)
+            //{ Console.WriteLine("Yes"); }
+            //else { Console.WriteLine("no"); }
             #endregion
 
-        }
+            #region Q2
+            //Write a program that allows the user to insert an integer then print
+            //negative if it is negative number otherwise print positive.
+            Console.WriteLine("Enter a Num");
+            double num2 = double.Parse(Console.ReadLine());
+            switch (num2)
+            {
+                case > 0:
+                    Console.WriteLine("It is a Positive Number");
+                    break;
+                case < 0:
+                    Console.WriteLine("It is a Negative  Number");
+                    break;
+                default:
+                    Console.WriteLine("It is azero "); break;
+            }
+
+
+                    #endregion
+
+            }
     }
 }
